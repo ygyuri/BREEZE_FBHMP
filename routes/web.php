1ef7fdb9223da459b96bce135448dae7bb12d22b
jharvine <jharvinekitiwa@gmail.com>
2024-12-30 13:21:42 +0300

    This is my signup
    have done set up and created a landing page

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,12 @@ use App\Http\Controllers\ProfileController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
+use App\Http\Controllers\Auth\EmailVerificationPromptController;
+
+Route::get('/email/verify', [EmailVerificationPromptController::class, '__invoke'])
+    ->middleware('auth')
+    ->name('verification.notice');
+
 
 Route::get('/', function () {
     return Inertia::render('Welcome', [
